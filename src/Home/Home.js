bc2bdcc2504bb21a4e2cef1e754ad66b3445931d
Farhan Elias <faarhaan10@gmail.com>
2021-10-04 15:20:48 +0600

    home carousel added

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,13 +1,42 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Carousel,Col,Container, Image, Row} from 'react-bootstrap';
+import HomeCarousel from '../HomeCarousel/HomeCarousel';
+import { useCourses } from '../hooks/useCourses';
 
 const Home = () => {
+    const [courses] = useCourses();
+
     return (
-        <Container>
-            <div>
-                <h1>this is home</h1>
-            </div>
-        </Container>
+        <div className="bg-info">
+            <Container>
+                <Carousel>
+                {
+                    courses.map(course => <Carousel.Item    
+                        interval={3000}
+                        key={course.id}>
+                        <Container>
+                        <Row className="align-items-center justify-content-center my-2">
+                            <Col xs={6}>
+                                <Image
+                                    className="d-block w-100 p-4"
+                                    src={course.imgUrl}
+                                    alt={course.courseName}
+                                    rounded
+                                />
+                            </Col>
+                            <Col xs={6} md={6}>
+                                   <div className="mx-auto my-5">
+                                   <h2>{course.courseName}</h2>
+                                    <h3>{course.institute}</h3>
+                                   </div>
+                            </Col>
+                        </Row>
+                        </Container>
+                    </Carousel.Item>)
+                }
+            </Carousel>
+            </Container>
+        </div>
     );
 };
 
