96160846ee07aeaf97ea77d6a6b5a7a29981afc9
Farhan Elias <faarhaan10@gmail.com>
2021-10-04 23:39:16 +0600

    404 fixed

--- src/componants/NotFound/NotFound.js
+++ src/componants/NotFound/NotFound.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Image, Row,Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
         <Container>
-                <div style={{width: '200px'}}>
-            <img src="https://www.knownhost.com/blog/wp-content/uploads/2015/07/404-page-example.jpg" alt="" />
-        </div>
-        </Container>
+                <Row className="align-items-center mt-5">
+                    
+                    <Col xs={12} md={6}>
+                        <Image className="img-fluid" src="https://i2.wp.com/learn.onemonth.com/wp-content/uploads/2017/08/1-10.png?fit=845%2C503&ssl=1"/>
+                    </Col>
+
+                    <Col xs={12}  md={6}>
+                        <div className="p-2">
+                            <h1>Oops....</h1>
+                            <h2>We can't seem to find the page you are looking for!</h2>
+                            <Link to="/">
+                                <Button>HOME</Button>
+                            </Link>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
     );
 };
 
