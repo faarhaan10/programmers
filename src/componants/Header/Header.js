ac5213c9ab015f78c9af33f577eb123a1acfb915
Farhan Elias <faarhaan10@gmail.com>
2021-10-05 16:27:35 +0600

    header updated

--- src/componants/Header/Header.js
+++ src/componants/Header/Header.js
@@ -4,27 +4,27 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
+// navigation 
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Container className="fw-bold">
 
-            <Navbar bg="dark" variant="dark" expand="lg">
-            <Container>
-
-                <Navbar.Brand as={Link} to="/" >PROGRAMMER</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/" className="fw-bolder fs-2 ">PROGRAMMER</Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
-                <Nav
-                    className="me-auto my-2 my-lg-0"
-                    style={{ maxHeight: '100px' }}
-                    navbarScroll
-                >
-                    <Nav.Link as={Link} to="/home" >Home</Nav.Link>
-                        <Nav.Link as={Link} to="/courses" >Courses</Nav.Link>
-                        <Nav.Link as={Link} to="/services" >Services</Nav.Link>
-                        <Nav.Link as={Link} to="/about" >About</Nav.Link>
-                        <Nav.Link as={Link} to="/contact" >Contact Us</Nav.Link>
-                </Nav>
+                    <Nav
+                        className="me-auto my-2 my-lg-0 fw-bold"
+                        style={{ maxHeight: '100px' }}
+                        navbarScroll
+                    >
+                            <Nav.Link as={Link} to="/home" >Home</Nav.Link>
+                            <Nav.Link as={Link} to="/courses" >Courses</Nav.Link>
+                            <Nav.Link as={Link} to="/services" >Services</Nav.Link>
+                            <Nav.Link as={Link} to="/about" >About</Nav.Link>
+                            <Nav.Link as={Link} to="/contact" >Contact Us</Nav.Link>
+                    </Nav>
                 </Navbar.Collapse>
             </Container>
-            </Navbar>
+        </Navbar>
     );
 };
 
